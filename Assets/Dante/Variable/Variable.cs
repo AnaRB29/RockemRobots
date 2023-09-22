6269a0c26c80fac0d809d34d2fcc66b4946e0e10
Dante <martin.velazquez.luis.alfonso@gmail.com>
2023-09-21 23:17:58 -0600

    Variable

--- Assets/Dante/Variable/Variable.cs
+++ Assets/Dante/Variable/Variable.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Variable : MonoBehaviour
+public abstract class Variable<T> : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
+    private T _value = default;
+    public T Value
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        get => _value;
+        set => _value = value;
     }
 }
