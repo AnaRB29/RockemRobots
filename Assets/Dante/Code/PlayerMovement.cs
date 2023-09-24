177d2b0e78737b3a8857087dd4cc80bd19ce75ee
Dante <martin.velazquez.luis.alfonso@gmail.com>
2023-09-24 17:12:44 -0600

    Player Scripts

---
+++ Assets/Dante/Code/PlayerMovement.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private InputActionProperty _movementAction;
+
+    private void Update()
+    {
+        //_movementAction.reference.asset
+    }
+}
