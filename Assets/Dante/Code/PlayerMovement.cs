f63dd363cbe18e94dbccbb8674dc4fc283aa8b89
Dante <martin.velazquez.luis.alfonso@gmail.com>
2023-09-24 19:43:40 -0600

    Changed Input

--- Assets/Dante/Code/PlayerMovement.cs
+++ Assets/Dante/Code/PlayerMovement.cs
@@ -1,16 +1,28 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Assertions;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private InputActionProperty _movementAction;
 
+    [Header("Config")] [SerializeField] private float _speed;
+
+    private Vector3 _vectorMove;
     private void Update()
     {
-        //_movementAction.reference.asset
+        GetInput();
+
+        Debug.Log($"Vector: {_movementAction.action.ReadValue<Vector2>()}");
+
+        var position = transform.position;
+        position += _vectorMove * _speed;
+        transform.position = position;
+    }
+
+    private void GetInput()
+    {
+        _vectorMove = _movementAction.action.ReadValue<Vector2>();
+        //_vectorMove.x = vectorMove.x;
+        //_vectorMove.z = vectorMove.y;
     }
 }
